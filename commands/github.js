2721cdb11b2418363766d721b390be712e7e8493
Evvy Xan <144759148+Evvyinnit@users.noreply.github.com>
2025-01-31 10:00:38 +0530

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -2,10 +2,10 @@ async function githubCommand(sock, chatId) {
     const repoInfo = `*🤖 KnightBot MD*
 
 *📂 GitHub Repository:*
-https://github.com/mruniquehacker/Knightbot-MD
+https://github.com/Evvyinnit/Knightbot-MD
 
 *📢 Official Channel:*
-https://youtube.com/@mr_unique_hacker
+https://youtube.com/@evvyxan
 
 _Star ⭐ the repository if you like the bot!_`;
 
