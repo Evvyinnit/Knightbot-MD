100b3c794138c6ee986d7bb94e2bb366e5bd3889
Evvy Xan <144759148+Evvyinnit@users.noreply.github.com>
2025-02-01 12:51:29 +0530

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,17 +1,25 @@
-const settings = require('../settings');
-
 async function ownerCommand(sock, chatId) {
+    const ownerName = "Nimesh D. Bandara"; // Change this to your name
+    const ownerNumber = "+94761206570"; // Change this to your WhatsApp number
+
     const vcard = `
 BEGIN:VCARD
 VERSION:3.0
-FN:${settings.botOwner}
-TEL;waid=${settings.ownerNumber}:${settings.ownerNumber}
+FN:${ownerName}
+TEL;waid=${ownerNumber}:${ownerNumber}
 END:VCARD
 `;
 
+    // Batman-Themed Message
+    const batmanQuote = `"The night is darkest just before the dawn. And I promise you, the dawn is coming." - Batman`;
+    
+    const styledMessage = `🦇 *THE DARK KNIGHT'S SIGNAL* 🦇\n\n${batmanQuote}\n\n📞 *Need the Bat? Contact my trusted ally:*`;
+
     await sock.sendMessage(chatId, {
-        contacts: { displayName: settings.botOwner, contacts: [{ vcard }] },
+        contacts: { displayName: ownerName, contacts: [{ vcard }] }
     });
+
+    await sock.sendMessage(chatId, { text: styledMessage });
 }
 
-module.exports = ownerCommand;
+module.exports = { ownerCommand };
